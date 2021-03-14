91ee0014e64d4a051c759f95b275c0545b3d21ed
Wolfgang Hottgenroth <wolfgang.hottgenroth@icloud.com>
2021-03-14 19:35:41 +0100

    upload tool

---
+++ tools/upload.sh
@@ -0,0 +1,1 @@
+esptool -p /dev/ttyUSB0 -b 115200 --before default_reset --after hard_reset --chip esp32  write_flash --flash_mode dio --flash_size detect --flash_freq 40m 0x1000 src/build/bootloader/bootloader.bin 0x8000 src/build/partition_table/partition-table.bin 0x10000 src/build/wifi_prov_mgr.bin
